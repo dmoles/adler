ab75e60f129aaf7a3c05d0250f233ef0ca483964
David Moles <dmoles@dmoles.net>
2020-11-05 20:46:26 -0800

    Add (unused) server cssDir property

--- main.go
+++ main.go
@@ -9,20 +9,18 @@ import (
 
 const defaultPort = 8181
 
-func start(port int, rootDir string) error {
-	return server.Start(port, rootDir)
-}
-
 func main() {
 	var port int
+	var cssDir string
 	var cmd = &cobra.Command{
 		Use:  "adler <rootDir>",
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return start(port, args[0])
+			return server.Start(port, args[0], cssDir)
 		},
 	}
 	cmd.Flags().IntVarP(&port, "port", "p", defaultPort, "port to listen on")
+	cmd.Flags().StringVar(&cssDir, "css", "", "CSS/SCSS directory (for testing)")
 	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
