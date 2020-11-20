e3ea035071de4634d9a894a052b5094b750f2636
David Moles <dmoles@dmoles.net>
2020-11-19 21:20:05 -0800

    Skip compiling/embedding unmodified SCSS/resources

--- magefile.go
+++ magefile.go
@@ -5,11 +5,14 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/bep/golibsass/libsass"
 	"github.com/get-woke/go-gitignore"
@@ -22,9 +25,12 @@ import (
 // Constants and const-like variables
 
 const projectName = "adler"
-const mainScssPath = "scss/main.scss"
+const mainCss = "resources/css/main.css"
+const mainScss = "scss/main.scss"
+const resourceDir = "resources"
+const statikData = "statik/statik.go"
 
-var scssDir = filepath.Dir(mainScssPath)
+var scssDir = filepath.Dir(mainScss)
 var gitIgnore = compileGitIgnore()
 
 // ------------------------------------------------------------
@@ -85,6 +91,11 @@ type Assets mg.Namespace
 func (Assets) Embed() error {
 	mg.Deps(Assets.Compile)
 
+	if !anyNewerThan(resourceDir, statikData) {
+		println("Assets are up to date")
+		return nil
+	}
+
 	includes := strings.Join([]string{
 		"*.css",
 		"*.ico",
@@ -97,7 +108,7 @@ func (Assets) Embed() error {
 	}, ",")
 
 	var statik = ensureCommand("statik", "statik not found; did you run go get github.com/rakyll/statik?")
-	cmd := exec.Command(statik, "-Z", "-src", "resources", "-include", includes, "-ns", projectName)
+	cmd := exec.Command(statik, "-Z", "-src", resourceDir, "-include", includes, "-ns", projectName)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -136,24 +147,26 @@ func (Assets) Validate() error {
 func (Assets) Compile() error {
 	mg.Deps(Assets.Validate)
 
-	outputDir := "resources/css"
-	outputPath := filepath.Join(outputDir, "main.css")
+	if !anyNewerThan(resourceDir, mainCss) {
+		println("CSS is up to date")
+		return nil
+	}
 
 	libsassOptions := libsass.Options{
 		IncludePaths: []string{scssDir},
 		OutputStyle:  libsass.ExpandedStyle,
 	}
 
-	println(fmt.Sprintf("Transpiling %v to %v", mainScssPath, outputPath))
+	println(fmt.Sprintf("Transpiling %v to %v", mainScss, mainCss))
 	if mg.Verbose() {
 		msg := fmt.Sprintf("libsass options: %#v", libsassOptions)
 		println(msg)
 	}
 
 	if mg.Verbose() {
-		println("Reading " + mainScssPath)
+		println("Reading " + mainScss)
 	}
-	mainScss, err := readFileAsString(mainScssPath)
+	mainScss, err := readFileAsString(mainScss)
 	if err != nil {
 		return err
 	}
@@ -165,21 +178,105 @@ func (Assets) Compile() error {
 	}
 
 	if mg.Verbose() {
-		println("Ensuring output directory " + outputDir)
+		println("Ensuring output directory " + ("resources/css"))
 	}
-	err = os.MkdirAll(outputDir, 0755)
+	err = os.MkdirAll("resources/css", 0755)
 	if err != nil {
 		return err
 	}
 
 	if mg.Verbose() {
-		println("Writing " + outputPath)
+		println("Writing " + mainCss)
 	}
-	return ioutil.WriteFile(outputPath, []byte(result.CSS), 0644)
+	return ioutil.WriteFile(mainCss, []byte(result.CSS), 0644)
 }
 
 // ------------------------------------------------------------
 // Helper functions
+
+// TODO: put all this timestamp business in a struct, or find a utility library for it (does mage have one?)
+
+var timeZero = time.Time{}
+
+func anyNewerThan(sourceDir string, targetFile string) bool {
+	targetModTime, err := effectiveModTime(targetFile)
+	if err != nil {
+		return true
+	}
+	if mg.Verbose() {
+		fmt.Printf("%v modified %v\n", targetFile, targetModTime)
+	}
+
+	err = filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		modTime, err := effectiveModTime(path)
+		if err != nil {
+			return err
+		}
+		if modTime.After(targetModTime) {
+			if mg.Verbose() {
+				fmt.Printf("%v modified %v\n", path, modTime)
+			}
+			return io.EOF
+		}
+		return nil
+	})
+	if err == nil {
+		return false
+	}
+	if err == io.EOF {
+		return true
+	}
+	panic(err)
+}
+
+func effectiveModTime(path string) (time.Time, error) {
+	if !tracked(path) || changed(path) {
+		return modTime(path)
+	}
+	return commitTime(path)
+}
+
+func tracked(path string) bool {
+	cmd := exec.Command("git", "ls-files", "--error-unmatch", path)
+	return cmd.Run() == nil
+}
+
+func changed(path string) bool {
+	cmd := exec.Command("git", "diff", "--quiet", path)
+	return cmd.Run() != nil
+}
+
+func modTime(path string) (time.Time, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return timeZero, err
+	}
+	return info.ModTime(), nil
+}
+
+func commitTime(path string) (time.Time, error) {
+	ts, err := commitTimestamp(path)
+	if err != nil {
+		return timeZero, err
+	}
+	return time.Unix(ts, 0), nil
+}
+
+func commitTimestamp(path string) (int64, error) {
+	cmd := exec.Command("git", "log", "-1", "--format=%ct", path)
+	tsBytes, err := cmd.Output()
+	if err != nil {
+		return 0, err
+	}
+	tsStr := strings.TrimSpace(string(tsBytes))
+	if len(tsStr) == 0 {
+		return 0, err
+	}
+	return strconv.ParseInt(tsStr, 10, 64)
+}
 
 func sassLint(scssFile string) error {
 	var sassLint = ensureCommand("sass-lint", "sass-lint not found; did you run npm install -g sass-lint?")
